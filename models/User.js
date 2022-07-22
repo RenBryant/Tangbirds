c6eff6d25d578ccdd6f3006957e5a989e2568038
RenBryant <renbee2bee@gmail.com>
2022-07-21 21:27:47 -0400

    added routes and fixed User model

--- models/User.js
+++ models/User.js
@@ -1,45 +1,61 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/config');
 const bcrypt = require('bcrypt');
+const sequelize = require('../config/config');
 
-class User extends Model {}
+// create User model
+class User extends Model {
+    checkPassword(loginpw) {
+        return bcrypt.compareSync(loginpw, this.password)
+    }
+}
 
 User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    }
-  },
-  {
-    hooks: {
-        beforeCreate: async(data) => {
-            data.password = await bcrypt.hash(data.password, 10)
-            return data
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
         },
-        beforeUpdate: async(data) => {
-            data.password = await bcrypt.hash(data.password, 10)
-            return data
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
         }
+    },
+    {
+        hooks: {
+            beforeCreate: async(data) => {
+                data.password = await bcrypt.hash(data.password, 10);
+                return data;
+            },
+            beforeUpdate: async(data) => {
+              data.password = await bcrypt.hash(data.password, 10);
+              return data;
+            }
+        },
+        
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
     }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'User',
-  }
 );
 
 module.exports = User;
--- models/index.js
+++ models/index.js
@@ -2,13 +2,13 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-Post.belongsTo(User, {
-  foreignKey: 'userId',
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
   onDelete: 'CASCADE'
 });
 
-Post.hasMany(Comment, {
-  foreignKey: 'postId',
+Post.belongsTo(User, {
+  foreignKey: 'userId',
   onDelete: 'CASCADE'
 });
 
